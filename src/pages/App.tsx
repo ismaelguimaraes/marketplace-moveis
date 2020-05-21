fa10d53b4a7a21c4106f79a646bdaa75048276b3
Ismael Guimarães <56137836+ismaelguimaraes@users.noreply.github.com>
2020-05-21 00:55:20 -0300

    Adicionando redux ao projeto

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from '../store/store'
 import Routes from './routes';
 
 interface Props {}
 
 const App: React.FC<Props> = () => {
   return (
-    <Router>
-      <Routes />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes />
+      </Router>
+    </Provider>
   );
 }
 
